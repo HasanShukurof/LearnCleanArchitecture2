8a2c990f67a997ea335be6900f46564a605c1a7a
Hasan Shukurov <hasan.shukurof@gmail.com>
2026-03-02 10:40:03 +0400

    fix

---
+++ app/src/main/java/com/example/learncleanarchitecture2/presentation/StartedLoginScreen.kt
@@ -0,0 +1,44 @@
+package com.example.learncleanarchitecture2.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlin.collections.get
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StartedLoginScreen() {
+    Surface() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ElevatedButton(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp),
+            ) { }
+        }
+    }
+}
